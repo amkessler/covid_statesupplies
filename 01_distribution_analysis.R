19cb6d2d3d4bb936cbaf1d827021409321f9640c
Kessler <ak113714@bna.com>
2020-04-10 15:35:35 -0400

    sorted column order and exported

--- 01_distribution_analysis.R
+++ 01_distribution_analysis.R
@@ -14,23 +14,62 @@ ppedata <- ppedata %>%
   select(
     name, state_or_local, casecount, censuspop2010, cases_per_100k,
     n95_respirators, ventilator
-  )
+  ) 
 
 
 #calculate ratios based on raw population (as feds did) and per_100k (as we did)
 #so that we can compare we'll add ranks, too
 
-ppedata %>% 
+ppedata <- ppedata %>% 
   mutate(
-    n95_bypop = n95_respirators / censuspop2010 * 100,
-    n95_bypop_rank = min_rank(n95_bypop)
-  ) %>% 
-  View()
-  
-  
-  
-  
-  
+    #by population per capita
+    n95_percapita = n95_respirators / censuspop2010,
+    n95_percapita_rank = min_rank(desc(n95_percapita)),
+    vent_percapita = ventilator / censuspop2010,
+    vent_percapita_rank = min_rank(desc(vent_percapita)),
+    #by total cases number
+    n95_bytotalcases = n95_respirators / casecount,
+    n95_bytotalcases_rank = min_rank(desc(n95_bytotalcases)),
+    vent_bytotalcases = ventilator / casecount,
+    vent_bytotalcases_rank = min_rank(desc(vent_bytotalcases)),
+    #by cases per 100k people
+    n95_bycases100k = n95_respirators / cases_per_100k,
+    n95_bycases100k_rank = min_rank(desc(n95_bycases100k)),
+    vent_bycases100K = ventilator / cases_per_100k,
+    vent_bycases100K_rank = min_rank(desc(vent_bycases100K)),
+    
+  ) 
 
+
+#reorder columns  
+names(ppedata)
+
+ppedata_final <- ppedata %>% 
+  select(
+    name,
+    state_or_local,
+    casecount,
+    censuspop2010,
+    cases_per_100k,
+    n95_respirators_DELIVERED = n95_respirators,
+    n95_percapita,
+    n95_percapita_rank,
+    n95_bytotalcases,
+    n95_bytotalcases_rank,
+    n95_bycases100k,
+    n95_bycases100k_rank,
+    ventilators_DELIVERED = ventilator,
+    vent_percapita,
+    vent_percapita_rank,
+    vent_bytotalcases,
+    vent_bytotalcases_rank,
+    vent_bycases100K,
+    vent_bycases100K_rank
+  )
+
+ppedata_final  
+
+#export for sharing
+write_xlsx(ppedata_final, "output/ppedata_export.xlsx")
 
 
