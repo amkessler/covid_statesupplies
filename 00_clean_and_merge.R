0f72e7ccfd3544a3e0a453f2077d5dffeed5b6ae
Aaron Kessler <kessler.aaron@gmail.com>
2020-04-10 23:26:36 -0400

    remove all hhs cleaning, moved to step 02 on its own instead

--- 00_clean_and_merge.R
+++ 00_clean_and_merge.R
@@ -5,14 +5,6 @@ library(tidycensus)
 library(readxl)
 library(writexl)
 
-# In this script, we'll load and clean up two separate datasets, one and then the other
-# The first is personal protective equipment (ppe) allocations sent to the states
-# The second is grant data on HHS grants made to state and local governments/organizations
-
-
-## 1) PPE DATA #############################
-
-
 #import ppe data
 ppe <- read_excel("data/SNS_PPE_REPORT.xlsx")
 
@@ -101,52 +93,5 @@ head(joined_ppe)
 
 #save results for next step
 saveRDS(joined_ppe, "data/joined_ppe.rds")
-
-
-
-
-#############################################################################
-
-# 2) HHS COVID-19 AWARD GRANT DATA ####
-
-# source here: https://taggs.hhs.gov/coronavirus
-
-
-#import the latest version of the data after downloaded as csv
-taggs_latest_raw <- read_csv("data/taggs_export_latest.csv", col_types = cols(.default = "c"))
-
-#clean names and format columns
-# **note: parse_number doesn't handle negative currency well, so because there's at least 
-# one negative dollar amount in the data we'll strip out the dollar sign ($) first
-taggs_latest <- taggs_latest_raw %>% 
-  clean_names() %>% 
-  mutate(
-    award_amount = str_remove(award_amount, "\\$"), 
-    award_amount = parse_number(award_amount),
-    action_date = mdy(action_date)
-  ) 
-
-names(taggs_latest)
-
-#let's use the joined table for ppe above to pull out the already good-to-go state case counts and pops
-tempdf <- joined_ppe %>% 
-  select(
-    name, state_or_local, casecount, censuspop2010, cases_per_100k
-  )
-
-#since the HHS data has only state abbreviations, not full names, we'll need to add that
-#we'll use tidycensus' built in tables to pull together
-statelist <- tidycensus::fips_codes %>% 
-  select(state, state_name) %>% 
-  distinct() %>% 
-  mutate(state_name = str_to_lower(state_name))
-
-tempdf <- left_join(tempdf, statelist, by = c("name" = "state_name")) 
-
-tempdf
-
-#ok now we're ready to join the temporary table back to our HHS grant data
-
-
 
 
